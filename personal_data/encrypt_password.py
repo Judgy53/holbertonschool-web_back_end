ae9befd3852024bac23a849d840d3a8408e13abd
Judgy53 <9182@holbertonstudents.com>
2024-06-17 16:16:42 +0200

    personal_data - Task 5: pycodestyle compliance

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,6 +5,7 @@ Module that provides helper functions to encrypt passwords
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """Salts and hash a password"""
     return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
