9b6320950d54658dcc25b9478b4468f4bbdc949b
Judgy53 <9182@holbertonstudents.com>
2024-06-18 15:56:20 +0200

    Basic_authentication - Task 9: User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 import base64
 import binascii
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -39,3 +40,21 @@ class BasicAuth(Auth):
             return decoded.decode("utf-8")
         except binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """Extracts the user credentials from the decoded header"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        separator_index = decoded_base64_authorization_header.find(":")
+        if separator_index == -1:
+            return (None, None)
+
+        email = decoded_base64_authorization_header[:separator_index]
+        password = decoded_base64_authorization_header[separator_index + 1:]
+        return (email, password)
