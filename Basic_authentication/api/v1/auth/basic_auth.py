ea49d8a706a690e2e721c45f11719f5294ca7a3b
Judgy53 <9182@holbertonstudents.com>
2024-06-18 15:41:34 +0200

    Basic_authentication - Task 8: base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Authorization module
 """
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +22,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Decodes the base64 part of the auth header"""
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header,
+                                       validate=True)
+            return decoded.decode("utf-8")
+        except binascii.Error:
+            return None
