731a30274fbc25e1f0cf21181d4f6a042a2de2b5
Judgy53 <9182@holbertonstudents.com>
2024-06-20 10:51:37 +0200

    Session_authentication - Task 5: Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -56,10 +56,12 @@ def before_request() -> str:
 
     if not auth.require_auth(request.path, ['/api/v1/stat*',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
         return None
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
 
     request.current_user = auth.current_user(request)
