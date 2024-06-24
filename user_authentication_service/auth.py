9dbae08e8cc46c1c3542c45f03bfc9056eb0a803
Judgy53 <9182@holbertonstudents.com>
2024-06-24 15:19:02 +0200

    user_auth_service - Task 9: Move method outside the class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -14,6 +14,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Generate a UUID4 as string"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -45,7 +50,3 @@ class Auth:
 
         return bcrypt.checkpw(password.encode("utf-8"),
                               user.hashed_password.encode("utf-8"))
-
-    def _generate_uuid(self) -> str:
-        """Generate a UUID4 as string"""
-        return str(uuid.uuid4())
