b8132f7eb01b6f59c25d83692da4b844ca4e653b
Judgy53 <9182@holbertonstudents.com>
2024-06-24 14:38:03 +0200

    user_auth_service - Task 7: Register user route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def index() -> str:
     """ GET /
-    Return:
+
+    Returns :
     - {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+
+    JSON body :
+        - email
+        - password
+
+    Returns :
+        - {"email": "<registered email>", "message": "user created"}
+        - if the email or password is missing:
+            - {"message": "email and password are required fields"}
+            - 400
+        - if the user is already registered:
+            - {"message": "email already registered"}
+            - 400
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if email is None or password is None:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
