c34d13bfc6c5b9375d8c40ca4c4adc74d0daaf22
Judgy53 <9182@holbertonstudents.com>
2024-07-15 14:24:31 +0200

    unittest_in_js - Task 5: Hooks

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+    let logSpy;
+
+    beforeEach(function() {
+        logSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(function() {
+        logSpy.restore();
+    });
+
+    it('logs the correct output with args: 100, 20', function() {
+        sendPaymentRequestToApi(100, 20);
+        expect(logSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    });
+
+    it('logs the correct output with args: 10, 10', function() {
+        sendPaymentRequestToApi(10, 10);
+        expect(logSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    });
+});
