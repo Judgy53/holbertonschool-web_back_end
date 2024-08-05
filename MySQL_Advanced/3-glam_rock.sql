13e18642be04cec58d61ae808ce6bbb0cd87f2e1
Judgy53 <9182@holbertonstudents.com>
2024-08-05 16:07:39 +0200

    mysql_advanced - Task 3: Use year 2020 as reference for longevity

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Script that lists all Glam rock bands, ranked by their longevity
 
 -- Query the bands and their longevity
-SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed as lifespan
+SELECT band_name, IFNULL(split, 2020) - formed as lifespan
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
